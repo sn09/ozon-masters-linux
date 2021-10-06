c1ea3790b71f1a4b3338b4c75dd74e5006d6b052
Сараев_Никита <lguest201@edge1.ru-central1.internal>
2021-10-06 19:54:13 +0300

    Resolved pull conflict

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 print("Hello, git")
 =======
 print("Hello, git")
 >>>>>>> 10fa95bf482ee8331da0f770015dc1ade72a39b8
+=======
+print("Hello, git!")
+>>>>>>> Stashed changes
